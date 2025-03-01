0fa86e7af4a62f8ed4fe02f27525c882f0f70f80
Mr. Ryt <flaviosobbin04@gmail.com>
2025-03-01 23:12:41 +0000

    handles wrong urls

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require("express-async-errors");
 
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const errorHandler = require("./handlers/errorHandler");
 require("dotenv").config();
 const app = express();
-app.use(cors)
+app.use(cors);
 
 const mongoose = require("mongoose");
 const userRoutes = require("./modules/users/user.routes");
@@ -35,6 +35,12 @@ app.get("/", (req, res) => {
 });
 app.use("/users", userRoutes);
 app.use("/transactions", transactionRoutes);
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    status: "Failed!",
+    message: "Page Not Found!",
+  });
+});
 
 // End of Routes
 app.use(errorHandler);
